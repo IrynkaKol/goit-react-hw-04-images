d95ffd6dae50b007429270ceff035d522502af1f
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-01-30 19:27:47 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,35 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalStyled } from './Modal.styled';
 const modalRef = document.querySelector('#modal-root');
 
+
+export function Modal ({ largeImg, onClose }) {
+  useEffect (() => {
+    const onCloseByEsc = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', onCloseByEsc);
+    return () => {
+      window.removeEventListener('keydown', onCloseByEsc);
+    }
+
+  }, [onClose])
+
+ 
+
+  return createPortal(
+    <Overlay onClick={onClose}>
+      <ModalStyled >
+        <img src={largeImg} alt="big" />
+      </ModalStyled>
+    </Overlay>,
+    modalRef
+  );
+}
+/*
 export class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.onCloseByEsc);
@@ -26,4 +53,4 @@ export class Modal extends Component {
       modalRef
     );
   }
-}
+}*/
